dcd250299d9fedb602e20cc21af6d4cd27694c3e
Antoni_Goszcz <antonigoszcz1996@gmail.com>
2025-04-27 18:15:31 +0200

    nowy szablon

--- blogAG/views.py
+++ blogAG/views.py
@@ -1,6 +1,7 @@
+from .models import Post
 from django.shortcuts import render
-
+from django.utils import timezone
 
 def post_list(request):
-
-    return render(request, 'blogAG/post_list.html', {})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blogAG/post_list.html', {'posts': posts})
