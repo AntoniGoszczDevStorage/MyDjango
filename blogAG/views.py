7bcb438d5e0b50901428328e8d90f5c55e83d5e2
Antoni_Goszcz <antonigoszcz1996@gmail.com>
2025-04-28 20:14:43 +0200

    kolejny szablon

--- blogAG/views.py
+++ blogAG/views.py
@@ -1,7 +1,11 @@
 from .models import Post
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'blogAG/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogAG/post_detail.html', {'post': post})
